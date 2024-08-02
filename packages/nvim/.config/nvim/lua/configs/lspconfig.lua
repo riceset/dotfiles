e498c4aba97d1f5bee215148a701423c7d23b47c
komeno <tkomeno@student.42tokyo.jp>
2024-08-02 13:47:00 +0900

    backup

--- packages/nvim/.config/nvim/lua/configs/lspconfig.lua
+++ packages/nvim/.config/nvim/lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "clangd", "pyright" }
+local servers = { "html", "cssls", "clangd", "pyright"}
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
